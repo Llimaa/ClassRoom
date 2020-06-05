ed92f518917fc8923ff7ded79f47de9c3655d95f
Marcos Lima <marcoslimadefatima@gmail.com>
2020-06-05 01:34:51 -0300

    testes para o objeto de valor Email

---
+++ ClassRoom.Tests/ValueObjects/EmailTests.cs
@@ -0,0 +1,34 @@
+﻿using ClassRoom.Domain.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassRoom.Tests.ValueObjects
+{
+    [TestClass]
+    public class EmailTests
+    {
+        [TestMethod]
+        [TestCategory("Email Tests")]
+        public void DeveRetornarFalseSeEmailForNulo()
+        {
+            var email = new Email(null);
+            Assert.AreEqual(false, email.Valid);
+        }
+
+        [TestMethod]
+        public void DeveRetornarFalseSeEmailEstiverEmBranco()
+        {
+            var email = new Email("");
+            Assert.AreEqual(false, email.Valid);
+        }
+
+        [TestMethod]
+        public void DeveRetornarVerdadeiraSeEmailForValido()
+        {
+            var email = new Email("lima@gmail.com");
+            Assert.IsTrue(email.Valid);
+        }
+    }
+}
