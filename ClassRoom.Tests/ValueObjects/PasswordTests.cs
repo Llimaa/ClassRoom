5198ba3684b558a5cf0f8a9514e0963b0e04dffd
Marcos Lima <marcoslimadefatima@gmail.com>
2020-06-07 02:58:56 -0300

    Tests do valor de objeto Password

---
+++ ClassRoom.Tests/ValueObjects/PasswordTests.cs
@@ -0,0 +1,41 @@
+﻿using ClassRoom.Domain.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassRoom.Tests.ValueObjects
+{
+    [TestClass]
+    [TestCategory("Tests Password")]
+    public class PasswordTests
+    {
+        [TestMethod]
+        public void DeveRetornarFalsoSeSenhaForNula()
+        {
+            var passwoord = new Password(null);
+            Assert.IsFalse(passwoord.Valid);
+        }
+
+        [TestMethod]
+        public void DeveRetornarFalsoSeSenhaForMenorQueSeis()
+        {
+            var password = new Password("12345");
+            Assert.IsFalse(password.Valid);
+        }
+
+        [TestMethod]
+        public void DeveRetornarFalsoQuandoSenhaMaiorQueDes()
+        {
+            var password = new Password("12345679999");
+            Assert.AreEqual(false, password.Valid);
+        }
+
+        [TestMethod]
+        public void DeveRetornarVerdadeiraQuandoSenhaValida()
+        {
+            var password = new Password("12398453");
+            Assert.IsTrue(password.Valid);
+        }
+    }
+}
